15c934db3cf44ad92963701b04cef024ec98736e
isoca7 <isoca7@gmail.com>
2024-04-08 19:36:22 +0200

    Sin desafio

---
+++ src/data.ts
@@ -0,0 +1,118 @@
+interface Reserva {
+  tipoHabitacion: "standard" | "suite";
+  desayuno: boolean;
+  pax: number;
+  noches: number;
+}
+  
+ export const reservas : Reserva[]= [
+    {
+      tipoHabitacion: "standard",
+      desayuno: false,
+      pax: 1,
+      noches: 3,
+    },
+    {
+      tipoHabitacion: "standard",
+      desayuno: false,
+      pax: 1,
+      noches: 4,
+    },
+    {
+      tipoHabitacion: "suite",
+      desayuno: true,
+      pax: 2,
+      noches: 1,
+    },
+  ];
+
+export  class Caso1 {
+    tipoHabitacion: 'standard' | 'suite'
+    precioTipoHabitacion: number
+    desayuno: boolean
+    precioDesayuno: number
+    pax: number
+    noches: number
+    iva: number
+    precioAdicionalPorPersona: number
+  
+    constructor(
+      tipoHabitacion: 'standard' | 'suite',
+      desayuno: boolean,
+      pax: number,
+      noches: number
+    ) {
+      this.tipoHabitacion = tipoHabitacion
+      switch (this.tipoHabitacion) {
+        case 'standard':
+          this.precioTipoHabitacion = 100
+          break
+        case 'suite':
+          this.precioTipoHabitacion = 150
+      }
+      this.desayuno = desayuno
+      this.precioDesayuno = 15
+      this.pax = pax
+      this.noches = noches
+      this.iva = 0.21
+      this.precioAdicionalPorPersona = 40
+    }
+  
+    calculoDesayuno() {
+      return this.desayuno ? this.precioDesayuno * this.noches : 0
+    }
+    calculoPrecioNoches() {
+      return this.precioTipoHabitacion * this.noches
+    }
+    calculoPersonasAdicionales() {
+      return this.pax > 1
+        ? this.precioAdicionalPorPersona * (this.pax-1) * this.noches
+        : 0
+    }
+    calculoIva() {
+      return this.subtotal() * this.iva
+    }
+    subtotal() {
+      return (
+        this.calculoPrecioNoches() +
+        this.calculoPersonasAdicionales() +
+        this.calculoDesayuno()
+      )
+    }
+    total() {
+      return this.subtotal() + this.calculoIva()
+    }
+  }
+  
+ export class Caso2 extends Caso1 {
+    descuento: number
+  
+    constructor(
+      tipoHabitacion: 'standard' | 'suite',
+      desayuno: boolean,
+      pax: number,
+      noches: number
+    ) {
+      super(tipoHabitacion, desayuno, pax, noches)
+      this.descuento = 0.15
+      this.precioTipoHabitacion = 100
+    }
+  
+    calculoDescuento(): number {
+      return (
+        (this.calculoPrecioNoches() +
+          this.calculoPersonasAdicionales() +
+          this.calculoDesayuno()) *
+        this.descuento
+      )
+    }
+    
+    subtotal(): number {
+      return (
+        this.calculoPrecioNoches() +
+        this.calculoPersonasAdicionales() +
+        this.calculoDesayuno() -
+        this.calculoDescuento()
+      )
+    }
+  }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,30 @@
-import "./style.css";
+import { reservas, Caso1, Caso2 } from './data'
 
-console.log("Hello Typescript!");
+
+
+for (let i = 0; i < reservas.length; i++) {
+  const reservaCaso1 = new Caso1(
+    reservas[i].tipoHabitacion,
+    reservas[i].desayuno,
+    reservas[i].pax,
+    reservas[i].noches
+  )
+  console.log(
+    `Caso 1: Total de la reserva numero ${i + 1}:`,
+    reservaCaso1.total(),
+    'Subtotal:',
+    reservaCaso1.subtotal()
+  )
+  const reservaCaso2 = new Caso2(
+    reservas[i].tipoHabitacion,
+    reservas[i].desayuno,
+    reservas[i].pax,
+    reservas[i].noches
+  )
+  console.log(
+    `Caso 2: Total de la reserva numero ${i + 1}:`,
+    reservaCaso2.total(),
+    'Subtotal:',
+    reservaCaso2.subtotal()
+  )
+}
