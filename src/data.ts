208971a3189f7f4b6c6bcb70b356a2768a91bce1
isoca7 <isoca7@gmail.com>
2024-04-11 14:07:09 +0200

    Total y subtotal de todas las reservas, hecho con reduce

--- src/data.ts
+++ src/data.ts
@@ -28,65 +28,50 @@ interface Reserva {
 
 export  class Caso1 {
     reservas : Reserva[]
-    tipoHabitacion: 'standard' | 'suite'
-    precioTipoHabitacion: number
-    desayuno: boolean
     precioDesayuno: number
-    pax: number
-    noches: number
+    precioPorNoche :number
+    precioPorPersonaAdicional:number
     iva: number
-    precioAdicionalPorPersona: number
-  
+
     constructor(
       reservas : Reserva[]
     ) {
-      this
-      reservas.forEach((reserva : Reserva)=>{
-        this.tipoHabitacion = reserva.tipoHabitacion
-      switch (this.tipoHabitacion) {
-        case 'standard':
-          this.precioTipoHabitacion = 100
-          break
-        case 'suite':
-          this.precioTipoHabitacion = 150
-      }
-      this.desayuno = reserva.desayuno
-      this.precioDesayuno = 15
-      this.pax = reserva.pax
-      this.noches = reserva.noches
-      this.iva = 0.21
-      this.precioAdicionalPorPersona = 40
-      })
-      
+      this.reservas = reservas
+      this.precioDesayuno=15
+      this.precioPorNoche=100
+      this.precioPorPersonaAdicional=40
+      this.iva=0.21
     }
   
-    calculoDesayuno() {
-      return this.desayuno ? this.precioDesayuno * this.noches : 0
+    calculaSubtotal() {
+      return this.reservas.reduce((subtotal, reserva)=>{
+        let totalDesayuno = 0
+        let totalPersonasAdicionales = 0
+        switch(reserva.tipoHabitacion){
+          case "standard":
+          this.precioPorNoche=100;
+          break
+          case "suite":
+            this.precioPorNoche=150
+        }
+        if(reserva.desayuno){
+         totalDesayuno = this.precioDesayuno * reserva.noches
+        }
+        if(reserva.pax>1){
+          totalPersonasAdicionales = ((reserva.pax-1)*this.precioPorPersonaAdicional)*reserva.noches
+        }
+        subtotal+= this.precioPorNoche*reserva.noches + totalDesayuno + totalPersonasAdicionales
+        return subtotal
+      },0)
     }
-    calculoPrecioNoches() {
-      return this.precioTipoHabitacion * this.noches
-    }
-    calculoPersonasAdicionales() {
-      return this.pax > 1
-        ? this.precioAdicionalPorPersona * (this.pax-1) * this.noches
-        : 0
-    }
-    calculoIva() {
-      return this.subtotal() * this.iva
-    }
-    subtotal() {
-      return (
-        this.calculoPrecioNoches() +
-        this.calculoPersonasAdicionales() +
-        this.calculoDesayuno()
-      )
-    }
-    total() {
-      return this.subtotal() + this.calculoIva()
+    calculaTotal() {
+      const subtotal = this.calculaSubtotal()
+      const impuestos = subtotal * this.iva
+      return subtotal + impuestos
     }
   }
   
- export class Caso2 extends Caso1 {
+ /*export class Caso2 extends Caso1 {
     descuento: number
   
     constructor(
@@ -109,7 +94,7 @@ export  class Caso1 {
       )
     }
     
-    subtotal(): number {
+    calculaSubtotal(): number {
       return (
         this.calculoPrecioNoches() +
         this.calculoPersonasAdicionales() +
@@ -117,4 +102,4 @@ export  class Caso1 {
         this.calculoDescuento()
       )
     }
-  }
+  }*/
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,7 @@
-import { reservas, Caso1, Caso2 } from './data'
+import { reservas, Caso1,  } from './data'
 
 
+const caso1 = new Caso1(reservas)
+console.log("Subtotal: ", caso1.calculaSubtotal())
+console.log("Total: ",caso1.calculaTotal())
 
-for (let i = 0; i < reservas.length; i++) {
-  const reservaCaso1 = new Caso1(
-    reservas[i].tipoHabitacion,
-    reservas[i].desayuno,
-    reservas[i].pax,
-    reservas[i].noches
-  )
-  console.log(
-    `Caso 1: Total de la reserva numero ${i + 1}:`,
-    reservaCaso1.total(),
-    'Subtotal:',
-    reservaCaso1.subtotal()
-  )
-  const reservaCaso2 = new Caso2(
-    reservas[i].tipoHabitacion,
-    reservas[i].desayuno,
-    reservas[i].pax,
-    reservas[i].noches
-  )
-  console.log(
-    `Caso 2: Total de la reserva numero ${i + 1}:`,
-    reservaCaso2.total(),
-    'Subtotal:',
-    reservaCaso2.subtotal()
-  )
-}
