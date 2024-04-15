46fad102386cf8fbf2083d999cc4fe4fd08e430c
isoca7 <isoca7@gmail.com>
2024-04-15 21:02:50 +0200

    caso 2 total añadido

--- src/data.ts
+++ src/data.ts
@@ -1,105 +1,108 @@
 interface Reserva {
-  tipoHabitacion: "standard" | "suite";
-  desayuno: boolean;
-  pax: number;
-  noches: number;
+  tipoHabitacion: 'standard' | 'suite'
+  desayuno: boolean
+  pax: number
+  noches: number
 }
-  
- export const reservas : Reserva[]= [
-    {
-      tipoHabitacion: "standard",
-      desayuno: false,
-      pax: 1,
-      noches: 3,
-    },
-    {
-      tipoHabitacion: "standard",
-      desayuno: false,
-      pax: 1,
-      noches: 4,
-    },
-    {
-      tipoHabitacion: "suite",
-      desayuno: true,
-      pax: 2,
-      noches: 1,
-    },
-  ];
 
-export  class Caso1 {
-    reservas : Reserva[]
-    precioDesayuno: number
-    precioPorNoche :number
-    precioPorPersonaAdicional:number
-    iva: number
+export const reservas: Reserva[] = [
+  {
+    tipoHabitacion: 'standard',
+    desayuno: false,
+    pax: 1,
+    noches: 3,
+  },
+  {
+    tipoHabitacion: 'standard',
+    desayuno: false,
+    pax: 1,
+    noches: 4,
+  },
+  {
+    tipoHabitacion: 'suite',
+    desayuno: true,
+    pax: 2,
+    noches: 1,
+  },
+]
 
-    constructor(
-      reservas : Reserva[]
-    ) {
-      this.reservas = reservas
-      this.precioDesayuno=15
-      this.precioPorNoche=100
-      this.precioPorPersonaAdicional=40
-      this.iva=0.21
-    }
-  
-    calculaSubtotal() {
-      return this.reservas.reduce((subtotal, reserva)=>{
-        let totalDesayuno = 0
-        let totalPersonasAdicionales = 0
-        switch(reserva.tipoHabitacion){
-          case "standard":
-          this.precioPorNoche=100;
-          break
-          case "suite":
-            this.precioPorNoche=150
-        }
-        if(reserva.desayuno){
-         totalDesayuno = this.precioDesayuno * reserva.noches
-        }
-        if(reserva.pax>1){
-          totalPersonasAdicionales = ((reserva.pax-1)*this.precioPorPersonaAdicional)*reserva.noches
-        }
-        subtotal+= this.precioPorNoche*reserva.noches + totalDesayuno + totalPersonasAdicionales
-        return subtotal
-      },0)
-    }
-    calculaTotal() {
-      const subtotal = this.calculaSubtotal()
-      const impuestos = subtotal * this.iva
-      return subtotal + impuestos
+export class Caso1 {
+  reservas: Reserva[]
+  precioDesayuno: number
+  precioPorNoche: number
+  precioPorPersonaAdicional: number
+  iva: number
+
+  constructor(reservas: Reserva[]) {
+    this.reservas = reservas
+    this.precioDesayuno = 15
+    this.precioPorNoche = 100
+    this.precioPorPersonaAdicional = 40
+    this.iva = 0.21
+  }
+
+  calculaPrecioHabitacionPorTipo(reserva: Reserva) {
+    switch (reserva.tipoHabitacion) {
+      case 'standard':
+        return 100 * reserva.noches
+      case 'suite':
+        return 150 * reserva.noches
     }
   }
-  
- /*export class Caso2 extends Caso1 {
-    descuento: number
-  
-    constructor(
-      tipoHabitacion: 'standard' | 'suite',
-      desayuno: boolean,
-      pax: number,
-      noches: number
-    ) {
-      super(tipoHabitacion, desayuno, pax, noches)
-      this.descuento = 0.15
-      this.precioTipoHabitacion = 100
+  calculaTotalDesayuno(reserva: Reserva) {
+    if (reserva.desayuno) {
+      return this.precioDesayuno * reserva.noches * reserva.pax
     }
-  
-    calculoDescuento(): number {
-      return (
-        (this.calculoPrecioNoches() +
-          this.calculoPersonasAdicionales() +
-          this.calculoDesayuno()) *
-        this.descuento
-      )
-    }
-    
-    calculaSubtotal(): number {
-      return (
-        this.calculoPrecioNoches() +
-        this.calculoPersonasAdicionales() +
-        this.calculoDesayuno() -
-        this.calculoDescuento()
-      )
-    }
-  }*/
+    return 0
+  }
+  calculaTotalPersonaAdicional(reserva: Reserva) {
+    return (reserva.pax - 1) * this.precioPorPersonaAdicional * reserva.noches
+  }
+
+  calculaSubtotal() {
+    return this.reservas.reduce((subtotal, reserva) => {
+      subtotal +=
+        this.calculaPrecioHabitacionPorTipo(reserva) +
+        this.calculaTotalDesayuno(reserva) +
+        this.calculaTotalPersonaAdicional(reserva)
+      return subtotal
+    }, 0)
+  }
+  calculaTotal() {
+    const subtotal = this.calculaSubtotal()
+    const impuestos = subtotal * this.iva
+    return subtotal + impuestos
+  }
+}
+
+export class Caso2 extends Caso1 {
+  descuento: number
+
+  constructor(reservas: Reserva[]) {
+    super(reservas)
+    this.descuento = 0.15
+  }
+
+  calculaPrecioHabitacionPorTipo(reserva: Reserva) {
+    return 100 * reserva.noches
+  }
+  calculoDescuento(): number {
+    return this.reservas.reduce((subtotal, reserva) => {
+      subtotal +=
+        this.calculaPrecioHabitacionPorTipo(reserva) +
+        this.calculaTotalDesayuno(reserva) +
+        this.calculaTotalPersonaAdicional(reserva)
+      return subtotal 
+    }, 0) * this.descuento
+  }
+
+  calculaSubtotal(): number {
+    return this.reservas.reduce((subtotal, reserva) => {
+      subtotal +=
+        this.calculaPrecioHabitacionPorTipo(reserva) +
+        this.calculaTotalDesayuno(reserva) +
+        this.calculaTotalPersonaAdicional(reserva) 
+      return subtotal 
+    }, 0) - this.calculoDescuento()
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { reservas, Caso1,  } from './data'
+import { reservas, Caso1, Caso2 } from './data'
 
 
 const caso1 = new Caso1(reservas)
-console.log("Subtotal: ", caso1.calculaSubtotal())
-console.log("Total: ",caso1.calculaTotal())
+console.log("Caso 1 Subtotal: ", caso1.calculaSubtotal())
+console.log("Caso 1 Total: ",caso1.calculaTotal())
 
+const caso2 = new Caso2(reservas)
+console.log("Caso 2 Subtotal: ", caso2.calculaSubtotal())
+console.log("Descuento: ", caso2.calculoDescuento())
+console.log("Caso 2 Total: ",caso2.calculaTotal())
